80d6eaa9cc3e5849609ddd1d149f45da0a876d58
Ujjawal Srivastava <srivastava.ujjawal011@gmail.com>
2022-05-08 15:20:10 +0530

    Bi-Directional Communication Established

--- chat/consumers.py
+++ chat/consumers.py
@@ -5,9 +5,9 @@ from channels.generic.websocket import WebsocketConsumer
 class ChatRoomConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
-        self.send(text_data=json.dumps({
-            'message': 'Established'
-        }))
 
     def receive(self, text_data=None):
         print('Message :', text_data)
+        self.send(text_data=json.dumps({
+            'message': 'Message Recieved'
+        }))
--- chat/static/js/chat.js
+++ chat/static/js/chat.js
@@ -2,6 +2,9 @@ const url = 'ws://' + window.location.host + '/ws/chat/';
 const chatSocket = new WebSocket(url);
 const chatSend = document.getElementById('message-form');
 
+// Getting Chat Log
+const chatLog = document.getElementById('chat-log');
+
 
 // When Socket Open
 chatSocket.onopen = function (event) {
@@ -10,7 +13,8 @@ chatSocket.onopen = function (event) {
 
 // When Socket Closed
 chatSocket.onmessage = function (event) {
-    console.log(event.data);
+    const recievedMessage = JSON.parse(event.data);
+    chatLog.innerHTML += `<div>${recievedMessage.message}</div>`;
 }
 
 chatSend.onsubmit = (event) => {
@@ -22,6 +26,7 @@ chatSend.onsubmit = (event) => {
 
     // Extracting the message and sending back to server
     let message = messageInput.value;
+    chatLog.innerHTML += `<div>${message}</div>`;
     chatSocket.send(message);
 
     // Resetting and setting back to focus
